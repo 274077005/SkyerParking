c187713e1971f3b4e560acb9b8c0f5a29ee8f939
274077005 <274077005@qq.com>
2019-08-19 23:50:58 +0800

    【更新忽略文件】

--- SkyerParking/AppDelegate/AppDelegate.mm
+++ SkyerParking/AppDelegate/AppDelegate.mm
@@ -24,6 +24,7 @@
 #import "skModShareConfig.h"
 #import "skIflyMSC.h"
 
+
 @interface AppDelegate ()
 
 @end
--- SkyerParking/Classes/首页模块/用户首页/Controller/skHomeViewController.m
+++ SkyerParking/Classes/首页模块/用户首页/Controller/skHomeViewController.m
@@ -235,7 +235,9 @@
 }
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+    });
 }
 
 - (void)didReceiveMemoryWarning {
